9e33646e69c678236134fa9da9a4451b09137203
licop <licop@users.noreply.github.com>
2023-02-19 16:58:48 +0800

    stroage二次封装

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,12 @@
 </template>
 
 <script>
+  
   export default {
-    name: 'App'
+    name: 'App',
+    mounted() {
+      this.$storage.setItem("name", "Tom")
+    }
   }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -6,9 +6,13 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import config from './config'
 import request from './util/request'
+import storage from './util/storage'
+
 
 const app = createApp(App)
 
 // 一个用于注册能够被应用内所有组件实例访问到的全局属性的对象。
 app.config.globalProperties.$request = request
+app.config.globalProperties.$storage = storage
+
 app.use(router).use(ElementPlus).mount('#app')
