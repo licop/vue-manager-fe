12b1d4791650a51d992d12ce9d914f83bb0fc31a
licop <licop@users.noreply.github.com>
2023-02-19 14:34:12 +0800

    feat:添加环境配置

---
+++ src/config/index.js
@@ -0,0 +1,26 @@
+/**
+ * 环境配置封装
+ */
+const env = import.meta.env.MODE || 'prod';
+
+const EnvConfig = {
+    dev:{
+        baseApi:'/api',
+        mockApi:'https://www.fastmock.site/mock/07aca2317968d5d1604e1f6872a3ec6a/api'
+    },
+    test:{
+        baseApi:'//test.futurefe.com/api',
+        mockApi:'https://www.fastmock.site/mock/07aca2317968d5d1604e1f6872a3ec6a/api'
+    },
+    prod:{
+        baseApi:'//futurefe.com/api',
+        mockApi:'https://www.fastmock.site/mock/07aca2317968d5d1604e1f6872a3ec6a/api'
+    }
+}
+
+export default {
+    env,
+    mock:false,
+    namespace:'manager',
+    ...EnvConfig[env]
+}
--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import axios from 'axios'
+import config from './config'
 
 const app = createApp(App)
-app.use(router).mount('#app')
+
+console.log(config, 12)
+axios.get(config.mockApi + '/login').then((res) => {
+  console.log(res)
+})
+
+app.use(router).use(ElementPlus).mount('#app')
